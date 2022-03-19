8d987c34ae858f4935b881e0e1ca7524572a2ba0
Twiliness <729294652hs@gmail.com>
2022-03-19 18:56:31 +0800

    collect_idiom.py
    * 尝试包含古诗词

--- collect_idiom.py
+++ collect_idiom.py
@@ -6,14 +6,40 @@ import hashlib
 words = []
 hash_set = set()
 
-with open("./assets/idiom.json", "r", encoding="utf-8") as ifp, \
+with open("./assets/THUOCL_poem.txt", "r", encoding="utf-8") as pfp,\
+        open("./assets/THUOCL_chengyu.txt", "r", encoding="utf-8") as ffp,\
+        open("./assets/idiom.json", "r", encoding="utf-8") as ifp, \
         open("./assets/idioms.json", "w", encoding="utf-8") as ofp:
+    poems = set([key.split()[0].strip() for key in pfp])
+
+    key = set([key.split()[0].strip() for key in ffp])
+
     doc = ujson.load(ifp)
+
+    for poem in poems:
+        word_hash = hashlib.sha1(poem.encode()).hexdigest()[0:8]
+
+        if word_hash in hash_set:
+            raise RuntimeError("duplicate hash")
+        else:
+            hash_set.add(word_hash)
+
+        words.append({
+            "hash": word_hash,
+            "word": poem,
+            "explanation": "无",
+            "pinyin": "",
+            "derivation": "无",
+            "type": "poem"
+        })
 
     for idiom in doc:
         word = idiom["word"]
 
         if len(word) != 4:
+            continue
+
+        if word not in key:
             continue
 
         word_hash = hashlib.sha1(word.encode()).hexdigest()[0:8]
@@ -28,7 +54,8 @@ with open("./assets/idiom.json", "r", encoding="utf-8") as ifp, \
             "word": word,
             "explanation": idiom["explanation"],
             "pinyin": idiom["pinyin"],
-            "derivation": idiom["derivation"]
+            "derivation": idiom["derivation"],
+            "type": "idiom"
         })
 
     ujson.dump(words, ofp, ensure_ascii=False)
