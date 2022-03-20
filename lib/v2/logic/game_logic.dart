418e8a04ede87c45550a6d98a875982676d6f914
Twiliness <729294652hs@gmail.com>
2022-03-20 23:03:21 +0800

    暂存记录

--- lib/v2/logic/game_logic.dart
+++ lib/v2/logic/game_logic.dart
@@ -1,5 +1,7 @@
 import 'package:wordle/v2/model/game_model.dart';
 
+import '../model/problem_model.dart';
+
 extension GameLogic on GameModel {
   void enterItem(InputItem item) {
     // 判断是否正在进行
@@ -31,5 +33,50 @@ extension GameLogic on GameModel {
     guessLogs[attempt][--cursor] = InputItem.empty();
 
     notifyListeners();
+  }
+
+  void setInputCharacterStatus(Character character, InputStatus status) {
+    for (var item in inputChoices) {
+      if (item.character == character) {
+        item.status = status;
+
+        return;
+      }
+    }
+  }
+
+  void checkInput() {
+    // 判断是否正在进行
+    if (gameStatus != GameStatus.statusRunning) {
+      return;
+    }
+
+    // 判断是否是某一行的末尾
+    if (cursor == (attempt + 1) * problem.length) {
+      return;
+    }
+
+    var answer = problem.chars;
+    var input = guessLogs[attempt];
+
+    for (var i = 0; i < answer.length; i++) {
+      if (answer[i] == input[i].character!) {
+        input[i].status = InputStatus.statusOk;
+
+        setInputCharacterStatus(answer[i], InputStatus.statusOk);
+      } else if (answer[i].char == input[i].character!.char) {
+        input[i].status = InputStatus.statusPartialCharacter;
+
+        setInputCharacterStatus(answer[i], InputStatus.statusPartialCharacter);
+      } else if (input.indexWhere((e) => e.character! == answer[i]) > 0) {
+        input[i].status = InputStatus.statusPartialPosition;
+
+        setInputCharacterStatus(answer[i], InputStatus.statusPartialPosition);
+      } else {
+        input[i].status = InputStatus.statusMissing;
+
+        setInputCharacterStatus(answer[i], InputStatus.statusMissing);
+      }
+    }
   }
 }
